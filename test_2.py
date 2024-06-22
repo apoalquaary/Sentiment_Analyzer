ede615e88d932bcd7e02bd94f9ae3ae6f4a7458f
apoalquaary <apoalquaary@gmail.com>
2024-06-22 13:22:36 +0300

    8

--- test_2.py
+++ test_2.py
@@ -1,38 +1,30 @@
 
-                     ###############################################################
-                    #                                                               #
-                    #                                                               #
-                    #                    Abdalrhman Alquaary                        #
-                    #  https://www.linkedin.com/in/abdalrhman-alquaary-2aa5a5233/   #
-                    #                                                               #
-                    #                                                               #
-                    #                                                               #
-                    #                                                               #
-                     ###############################################################
+ ###############################################################
+#                                                               #
+#                                                               #
+#                    Abdalrhman Alquaary                        #
+#  https://www.linkedin.com/in/abdalrhman-alquaary-2aa5a5233/   #
+#                                                               #
+#                                                               #
+#                                                               #
+#                                                               #
+ ###############################################################
 
 
-import pandas as pd
-import numpy as np
 from sentiment_analyzer import Sentiment_Analyzer
 from utils.data_pipeline import Data_Pipeline
 
-
-
 def main():
 
-	data, cleaned_data = Data_Pipeline.fetch_data()
+	texts = [
+		'You have a good heart',
+		"he didn't seem very well"
+	]
 
-	
-
-
-	sa = Sentiment_Analyzer(data, cleaned_data)
+	sa = Sentiment_Analyzer(texts)
 	sa.get_sentiments()
-
-
-
 
 if __name__ == '__main__':
 	main()
-	print("Done")
 
 
